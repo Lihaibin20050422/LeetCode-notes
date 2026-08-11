657723f980da1deb2ba0a28095a8ff4f8b5de8dc
李海彬 <1929845261@qq.com>
2026-08-11 12:07:58 +0800

    feat:完成两数之和 1号题，整理基础目录结构

---
+++ solutions/array/two-sum/1-two-sum.cpp
@@ -0,0 +1,20 @@
+/*
+给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+
+你可以假设每种输入只会对应一个答案，并且你不能使用两次相同的元素。
+*/
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return {i, j};
+                }
+            }
+        }
+        return {};  // 题目保证有解，不会走到这里
+    }
+};
